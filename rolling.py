c478cbe51e7424cef757ad52a9ad0b906d1af7a9
mjjecmen <mjjecmen@umich.edu>
2021-12-22 02:25:59 -0500

    added file io

--- rolling.py
+++ rolling.py
@@ -1,7 +1,23 @@
+import sys
 import spotipy
 import pylast
 import json
 import datetime
+from pathlib import Path
+
+# debug flag
+debug = False
+
+# constants
+LONG_DELIMITER = '[|||]'
+SHORT_DELIMITER = '[|]'
+ADDED = '[+]'
+REMOVED = '[-]'
+DATE_FORMAT = "%Y-%m-%d"
+
+def log(s):
+    if debug:
+        print(s)
 
 def get_config():
     # IMPORTANT: config.json is the only thing that's .gitignore'd
@@ -76,7 +92,7 @@ def get_rolling_tracklist(config, spotify):
                     "artists": [ artist['name'] for artist in spotify_track['artists'] ],
                     "album": spotify_track['album']['name']
                 }
-                # print(track)
+                log(track)
                 tracklist.append(track)
                 
             spotify_tracks = spotify.next(spotify_tracks)
@@ -86,34 +102,147 @@ def get_rolling_tracklist(config, spotify):
 
     return tracklist
 
-# for each track, get listens in past year by unix timestamp # TODO change this function
-def append_listens(tracks, lastfm):
-    for track in tracks:
-        print("LISTENS FOR TRACK", track["name"])
+def file_exists(filename):
+    return Path(filename).exists()
+
+# load previous tracklist from json file in config
+def load_previous_tracklist(config):
+    if not file_exists(config["STORAGE_FILENAME"]):
+        log("first time running this program, previous tracklist not stored yet")
+        return {}
+
+    with open(config["STORAGE_FILENAME"], "r") as trackfile:
+        return json.load(trackfile)
+
+def are_tracks_same(new, old):
+    # new have "name", "artists", and "album" fields
+    # old have "name", "artists", "album", and "playcount" fields
+    if new["name"] != old["name"]:
+        return False
+    if new["artists"] != old["artists"]:
+        return False
+    if new["album"] != old["album"]:
+        return False
+
+    return True
+
+# linear, who cares. returns track or None if not found
+def get_corresponding_track(tracklist, track):
+    for new_track in tracklist:
+        if are_tracks_same(new_track, track):
+            return track
+    return None
+
+# for each new track, get number of plays at present and store.
+# for each track which was removed from tracklist, get number of plays
+# at present and deduct plays when added to get plays since added.
+# returns updated tracklist, removed tracks as a pair
+def update_tracklist(new_tracklist, tracklist, date, lastfm):
+    # separate new tracks and kept tracks
+    news = []
+    kept = []
+    for new_track in new_tracklist:
+        existing_track = get_corresponding_track(tracklist, new_track)
+        if existing_track is None:
+            news.append(new_track)
+        else:
+            kept.append(existing_track)
+
+    # now repeat in reverse to find removed tracks
+    removed = []
+    for existing_track in tracklist:
+        if get_corresponding_track(new_tracklist, existing_track) is None:
+            # this track must have been removed since we last checked
+            removed.append(existing_track)         
+
+    # go through olds, update playcounts and timestamp out
+    for track in removed:
         scrobs = lastfm.get_track_scrobbles(track["artists"][0], track["name"])
-        listens = []
-        for played_track in scrobs:
-            listens.append({
-                "playback_date": played_track.playback_date,
-                "timestamp": played_track.timestamp
-            })
-        print(listens)
+        track["playcount"] = len(scrobs) - track["playcount"]
+        track["date_out"] = date
+
+    # go through news, set playcounts and timestamp in, and append to kept
+    for track in news:
+        scrobs = lastfm.get_track_scrobbles(track["artists"][0], track["name"])
+        track["playcount"] = len(scrobs)
+        track["date_in"] = date
+        kept.append(track)
+
+    # kept is now the updated current tracklist
+    return kept, removed, news
+
+# writes the track to the file provided in a consistent format
+def encode_track(track):
+    encoded = ""
+    encoded += LONG_DELIMITER + track["name"] + LONG_DELIMITER
+    for artist in track["artists"]:
+        encoded += artist + SHORT_DELIMITER
+    encoded += LONG_DELIMITER
+    encoded += track["album"] + LONG_DELIMITER
+    return encoded
+
+# if it does not already exist, create logfile
+def create_logfile(config, tracklist):
+    if file_exists(config["LOG_FILENAME"]):
+        return
+
+    # create logfile and store current 25 tracks in it
+    with open(config["LOG_FILENAME"], "w") as logfile:
+        logfile.write("STARTING TRACKS\n")
+        for track in tracklist:
+            logfile.write(encode_track(track) + '\n')
+        logfile.write('\n')
+
+def append_to_log(config, removed, added, date):
+    # no appending needed if no tracks were removed
+    # also, skip the day altogether if there wasn't an equal exchange
+    if len(removed) == 0 or len(removed) != len(added):
+        return False
+
+    # add diff to logfile
+    with open(config["LOG_FILENAME"], "a") as logfile:
+        logfile.write(LONG_DELIMITER + date + LONG_DELIMITER)
+        for rtrack in removed:
+            logfile.write(REMOVED)
+            logfile.write(encode_track(rtrack))
+            logfile.write(rtrack["playcount"] + LONG_DELIMITER + '\n')
+        for atrack in added:
+            logfile.write(ADDED)
+            logfile.write(encode_track(atrack) + '\n')
+        logfile.write('\n')
+
+    return True
+
+def write_tracklist_file(config, tracklist): # TODO have starting-songs.json and rolling log which is list of json substitutions
+    with open(config["STORAGE_FILENAME"], "w") as tfile:
+        if debug:
+            json.dump(tracklist, tfile, indent=4, sort_keys=True)
+        else:
+            json.dump(tracklist, tfile)
 
 def main():
     config = get_config()
     spotify, lastfm = authenticate_services(config)
 
-    tracks = get_rolling_tracklist(config, spotify)
+    # get current tracks and compare to previously stored tracks
+    tracklist = get_rolling_tracklist(config, spotify)
 
-    # TODO function which checks previous tracklist and writes a log with substitutions
-    # and timestamps added, timestamps removed, and plays since added
+    # read previous tracklist from storage file
+    previous_tracklist = load_previous_tracklist(config)
 
-    # TODO this should only be called at end of year
-    tracks = append_listens(tracks, lastfm)
+    # get diff and update playcounts for new and removed songs
+    date = datetime.datetime.today().strftime(DATE_FORMAT)
+    tracklist, removed, added = update_tracklist(tracklist, previous_tracklist, date, lastfm)
 
-    # TODO dump data to a json file, reload and update on each execution
-    # TODO end of year function which runs once and emails a file
-    # TODO email me for error reporting
+    # create logfile if does not exist
+    create_logfile(config, tracklist)
+
+    # finally, write the log and tracklist file to be checked next time
+    append_to_log(config, removed, added, date)
+    write_tracklist_file(config, tracklist)
 
 if __name__ == '__main__':
+    # debug printing on for any invocation with more than the required args
+    if len(sys.argv) > 2:
+        debug = True
     main()
